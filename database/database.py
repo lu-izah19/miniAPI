5cc00bc1b2f8799a42c4c338a1eb8e197af870f8
Luiza Souza Teixeira <luizasouzateixeira9@gmail.com>
2026-09-16 10:05:29 -0300

    Use MARIADB_HOST for database connection

--- database/database.py
+++ database/database.py
@@ -1,8 +1,8 @@
 import mysql.connector
-from config import MARIADB_USER, MARIADB_PASSWORD, MARIADB_DATABASE
+from config import MARIADB_HOST, MARIADB_USER, MARIADB_PASSWORD, MARIADB_DATABASE
 
 conexao = mysql.connector.connect(
-    host="137.131.133.237",
+    host=MARIADB_HOST,
     user=MARIADB_USER,
     password=MARIADB_PASSWORD,
     database=MARIADB_DATABASE
