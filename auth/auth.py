985c846957762a9af2d89236f78f559070aee89a
Luiza Souza Teixeira <luizasouzateixeira9@gmail.com>
2026-09-18 12:57:31 -0300

    Implement user validation with JWT in auth.py
    Added JWT validation for user credentials and imported necessary modules.

--- auth/auth.py
+++ auth/auth.py
@@ -1,6 +1,9 @@
 import logging
 from cryptography.fernet import Fernet
-from config import FERNET_KEY
+from config import FERNET_KEY, SECRET_KEY
+import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
 
 fernet = Fernet(FERNET_KEY)
 
@@ -13,3 +16,15 @@ def criptografar_email(email):
 def descriptografar_email(email):
     logging.info(f"Descriptografando email: {email}")
     return fernet.decrypt(email.encode('utf-8')).decode('utf-8')
+
+
+def validar_usuario(credenciais=Depends(HTTPBearer())):
+    try:
+        payload = jwt.decode(credenciais.credentials, SECRET_KEY, algorithms=["HS256"])
+        return payload
+    except jwt.ExpiredSignatureError:
+        logging.error("Token expirado!")
+        raise HTTPException(status_code=401, detail="Token expirado!")
+    except jwt.InvalidTokenError:
+        logging.error("Token inválido!")
+        raise HTTPException(status_code=401, detail="Token inválido!")
